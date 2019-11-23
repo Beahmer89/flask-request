7f6f66d461af6c7a58894c1adc1e6117d22e7240
Dj <Beahmer89@gmail.com>
2019-11-22 20:02:10 -0800

    Base Project - add test to assure application gets installed correctly

--- tests/test.py
+++ tests/test.py
@@ -3,12 +3,18 @@ from flask import Flask
 
 from tests import fixtures
 
+import flask_request
+
 
 class TestApplication(unittest.TestCase):
 
     def setUp(self):
         app = fixtures.application()
         self.test_client = app.test_client()
+
+    def test_extension_installed(self):
+        self.assertIsInstance(self.test_client.application.request,
+                              flask_request.RequestsSession)
 
     def test_extension_installed_and_works(self):
         response = self.test_client.get('/success')
